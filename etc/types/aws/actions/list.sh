fcfce68039daa8e9049c3ed7280b5101887cdd70
Matt Ruddy <matt@calcwise.com>
2023-02-23 12:08:49 +0000

    Add AWS-specific list and pull

--- etc/types/aws/actions/list.sh
+++ etc/types/aws/actions/list.sh
@@ -1,1 +1,3 @@
-echo "Listing"
+bucket=$1
+prefix=$2
+$flight_SILO_types/aws/cli/bin/aws s3api list-objects-v2 --bucket "$bucket" --no-sign-request --prefix "$prefix" --delimiter /
--- etc/types/aws/actions/pull.sh
+++ etc/types/aws/actions/pull.sh
@@ -1,1 +1,4 @@
-echo "Pulling"
+bucket=$1
+object=$2
+destination=$3
+$flight_SILO_types/aws/cli/bin/aws s3api get-object --bucket "$bucket" --key "$object" $destination --no-sign-request
