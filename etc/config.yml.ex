e8c75f3e32e94eb7dbf1453e215592c9430c701f
Matt Ruddy <matt@calcwise.com>
2023-02-24 11:02:06 +0000

    Add example config

---
+++ etc/config.yml.ex
@@ -0,0 +1,14 @@
+---
+# Path(s) where provider type metadata is stored
+#type_paths:
+#- "/opt/flight/opt/silo/etc/types"
+#
+# Path where global silos are stored
+#global_silos_path: "/opt/flight/opt/silo/etc/global"
+#
+# Metadata for public silos
+#public_silos:
+#- name: "flight-storage-test"
+#  description: "An example silo to demonstrate Flight Silo"
+#  type: "aws"
+#  is_public: true
