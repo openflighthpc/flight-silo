be58a2d862d4104cbdea764591327a8bc7820158
Matt Ruddy <matt@calcwise.com>
2023-03-03 16:22:41 +0000

    WIP: allow operations on private repos

--- etc/types/aws/actions/pull.sh
+++ etc/types/aws/actions/pull.sh
@@ -1,5 +1,7 @@
 object_uri="s3://$1$2"
 destination=$3
-region=$4
-recursive=$5
-$flight_SILO_types/aws/cli/bin/aws s3 cp "$object_uri" "$destination" --no-sign-request --region "$region" $5
+export AWS_DEFAULT_REGION=$4
+export AWS_ACCESS_KEY_ID=$5
+export AWS_SECRET_ACCESS_KEY=$6
+recursive=$7
+$flight_SILO_types/aws/cli/bin/aws s3 cp "$object_uri" "$destination" $recursive
