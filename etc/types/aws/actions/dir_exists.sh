299556ebb68e2d3c8af0fd84ac3bf4c35425021f
Matt Ruddy <matt@calcwise.com>
2023-03-07 13:54:12 +0000

    Fix `dir_exists` for private repos

--- etc/types/aws/actions/dir_exists.sh
+++ etc/types/aws/actions/dir_exists.sh
@@ -1,7 +1,10 @@
 bucket=$1
 key=${2:1}
-region=$3
-not_exist=$($flight_SILO_types/aws/cli/bin/aws s3api head-object --bucket "$bucket" --key "$key" --no-sign-request --region "$region">/dev/null 2>&1; echo $?)
+export AWS_DEFAULT_REGION=$3
+export AWS_ACCESS_KEY_ID=$4
+export AWS_SECRET_ACCESS_KEY=$5
+sign_request=$6
+not_exist=$($flight_SILO_types/aws/cli/bin/aws s3api head-object --bucket "$bucket" --key "$key" --region "$region" $6 >/dev/null 2>&1; echo $?)
 if [ $not_exist == 254 ]; then
   echo "no"
 else
