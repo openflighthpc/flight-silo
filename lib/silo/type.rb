7f01f7b93c47b2d79f8799e6da81e389c537663b
VoxSecundus <millard64@hotmail.co.uk>
2023-03-08 10:45:39 +0000

    Change how we fetch/set state

--- lib/silo/type.rb
+++ lib/silo/type.rb
@@ -38,10 +38,25 @@ module FlightSilo
       # TODO
     end
 
+    def state_file
+      File.join(@dir, 'state.yaml')
+    end
+
+    def state
+      YAML.load_file(state_file) || {}
+    end
+
+    def modify_state(&block)
+      modified = yield state
+      p modified
+      File.open(state_file, 'w') { |f| f.write modified.to_yaml }
+    end
+
     def set_prepared
-      state = YAML.load_file(File.join(dir, 'state.yaml'))
-      state[:prepared] = true
-      File.open(File.join(dir, 'state.yaml'), "w") { |file| file.write(state.to_yaml) }
+      modify_state do |s|
+        s.tap { |h| h[:prepared] = true }
+      end
+
       @prepared = true
     end
 
@@ -51,15 +66,9 @@ module FlightSilo
       @name = md[:name]
       @description = md[:description]
       @dir = dir
-      state_file = File.join(@dir, "state.yaml")
-      if File.exists?(state_file)
-        @prepared = YAML.load_file(state_file)[:prepared]
-      else
-        File.open(state_file, "w") do |out|
-          YAML.dump({prepared: false}, out)
-        end
-        @prepared = false
-      end
+
+      FileUtils.touch(state_file)
+      @prepared = !!state[:prepared]
     end
   end
 end
