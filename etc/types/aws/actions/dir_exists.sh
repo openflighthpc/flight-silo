ed9837c4d7a3e8e3d411121300a437186dba51d7
Matt Ruddy <matt@calcwise.com>
2023-03-09 14:56:14 +0000

    Make `dir_exists` general

--- etc/types/aws/actions/dir_exists.sh
+++ etc/types/aws/actions/dir_exists.sh
@@ -1,10 +1,10 @@
 bucket=$1
-key=${2:1}
-export AWS_DEFAULT_REGION=$3
-export AWS_ACCESS_KEY_ID=$4
-export AWS_SECRET_ACCESS_KEY=$5
-sign_request=$6
-not_exist=$($flight_SILO_types/aws/cli/bin/aws s3api head-object --bucket "$bucket" --key "$key" --region "$region" $6 >/dev/null 2>&1; echo $?)
+test $2 = true && sign_request=--no-sign-request || sign_request=""
+key=${3:1}
+export AWS_DEFAULT_REGION=$4
+export AWS_ACCESS_KEY_ID=$5
+export AWS_SECRET_ACCESS_KEY=$6
+not_exist=$($flight_SILO_types/aws/cli/bin/aws s3api head-object --bucket "$bucket" --key "$key" $sign_request --output json >/dev/null 2>&1; echo $?)
 if [ $not_exist == 254 ]; then
   echo "no"
 else
