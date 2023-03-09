fc01f8463f2ae59fa50a02be1be4a9265158c292
Matt Ruddy <matt@calcwise.com>
2023-03-09 15:00:43 +0000

    Make `list` general

--- etc/types/aws/actions/list.sh
+++ etc/types/aws/actions/list.sh
@@ -1,7 +1,7 @@
 bucket=$1
-prefix=${2:1}
-export AWS_DEFAULT_REGION=$3
-export AWS_ACCESS_KEY_ID=$4
-export AWS_SECRET_ACCESS_KEY=$5
-sign_request=$6
+test $2 = true && sign_request=--no-sign-request || sign_request=""
+prefix=${3:1}
+export AWS_DEFAULT_REGION=$4
+export AWS_ACCESS_KEY_ID=$5
+export AWS_SECRET_ACCESS_KEY=$6
 $flight_SILO_types/aws/cli/bin/aws s3api list-objects-v2 --bucket "$bucket" --prefix "$prefix" --delimiter / --output json $sign_request
