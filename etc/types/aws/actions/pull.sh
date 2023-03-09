2b5c708576b6be143859ad90a19640497ac0cd37
Matt Ruddy <matt@calcwise.com>
2023-03-09 15:01:34 +0000

    Make `pull` general

--- etc/types/aws/actions/pull.sh
+++ etc/types/aws/actions/pull.sh
@@ -1,7 +1,8 @@
-object_uri="s3://$1$2"
-destination=$3
-export AWS_DEFAULT_REGION=$4
-export AWS_ACCESS_KEY_ID=$5
-export AWS_SECRET_ACCESS_KEY=$6
-recursive=$7
-$flight_SILO_types/aws/cli/bin/aws s3 cp "$object_uri" "$destination" $recursive
+object_uri="s3://$1$3"
+test $2 = true && sign_request=--no-sign-request || sign_request=""
+destination=$4
+test $5 = true && recursive=--recursive || recursive=""
+export AWS_DEFAULT_REGION=$6
+export AWS_ACCESS_KEY_ID=$7
+export AWS_SECRET_ACCESS_KEY=$8
+$flight_SILO_types/aws/cli/bin/aws s3 cp "$object_uri" "$destination" $sign_request $recursive
