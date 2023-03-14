fb63df77d0833b67f77166c64fa6c60fc44f0b43
VoxSecundus <millard64@hotmail.co.uk>
2023-03-14 22:21:53 +0000

    Update file_exists action to use new format

--- etc/types/aws/actions/file_exists.sh
+++ etc/types/aws/actions/file_exists.sh
@@ -1,10 +1,6 @@
-bucket=$1
-test $2 = true && sign_request=--no-sign-request || sign_request=""
-key=${3:1}
-export AWS_DEFAULT_REGION=$4
-export AWS_ACCESS_KEY_ID=$5
-export AWS_SECRET_ACCESS_KEY=$6
-not_exist=$($flight_SILO_types/aws/cli/bin/aws s3api head-object --bucket "$bucket" --key "$key" $sign_request --output json >/dev/null 2>&1; echo $?)
+test $SILO_PUBLIC = true && sign_request=--no-sign-request || sign_request=""
+not_exist=$($SILO_TYPE_DIR/cli/bin/aws s3api head-object --bucket "$SILO_NAME" --key "$SILO_PATH" $sign_request --output json >/dev/null 2>&1; echo $?)
+
 if [ $not_exist == 254 ]; then
   echo "no"
 else
