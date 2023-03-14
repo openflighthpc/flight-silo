e0da548371506df7693181030cb4a1635c19ecb0
Matt Ruddy <matt@calcwise.com>
2023-03-14 15:20:59 +0000

    Move id work into own branch

--- etc/types/aws/actions/get_silo.sh
+++ etc/types/aws/actions/get_silo.sh
@@ -1,12 +1,6 @@
+<<<<<<< HEAD
 #!/bin/bash
 data=$($SILO_TYPE_DIR/cli/bin/aws s3api list-buckets --output json)
-buckets=($(echo $data | 
-  sed -e 's/[{}]/''/g' | 
-    awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |
-      sed -n -e 's/^.*Name": //p' |
-        grep flight-silo- |
-          cut -d'"' -f2))
-echo $buckets
 
 for bucket in ${buckets[@]}; do
   can_access=$($SILO_TYPE_DIR/cli/bin/aws s3api head-bucket --bucket $bucket >/dev/null 2>&1; echo $?)
