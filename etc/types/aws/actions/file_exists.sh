2f5e4128c70cbe6337c0db490fd95b4462627408
VoxSecundus <millard64@hotmail.co.uk>
2023-03-14 23:20:17 +0000

    Update file pull to use new format

--- etc/types/aws/actions/file_exists.sh
+++ etc/types/aws/actions/file_exists.sh
@@ -1,7 +1,7 @@
 test $SILO_PUBLIC = true && sign_request=--no-sign-request || sign_request=""
 not_exist=$($SILO_TYPE_DIR/cli/bin/aws s3api head-object --bucket "$SILO_NAME" --key "$SILO_PATH" $sign_request --output json >/dev/null 2>&1; echo $?)
 
-if [ $not_exist == 254 ]; then
+if [ $not_exist != 0 ]; then
   echo "no"
 else
   echo "yes"
