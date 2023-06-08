93f8c19168e163a3ba1da7851db248cf1f4206e0
VoxSecundus <millard64@hotmail.co.uk>
2023-06-08 14:37:39 +0100

    Print metadata to /tmp when creating repo

--- etc/types/aws/actions/create.sh
+++ etc/types/aws/actions/create.sh
@@ -5,6 +5,6 @@ $SILO_TYPE_DIR/cli/bin/aws s3 mb s3://$bucket
 $SILO_TYPE_DIR/cli/bin/aws s3api put-object --bucket $bucket --key files/
 $SILO_TYPE_DIR/cli/bin/aws s3api put-object --bucket $bucket --key projects/
 $SILO_TYPE_DIR/cli/bin/aws s3api put-object --bucket $bucket --key software/
-printf -- "---\nname: $SILO_NAME\ndescription:\nis_public: false\n" > silo_cloud_md.yaml
-$SILO_TYPE_DIR/cli/bin/aws s3api put-object --bucket $bucket --key cloud_metadata.yaml --body ./silo_cloud_md.yaml
-rm -f silo_cloud_md.yaml
+printf -- "---\nname: $SILO_NAME\ndescription:\nis_public: false\n" > "/tmp/${SILO_ID}_cloud_md.yaml"
+$SILO_TYPE_DIR/cli/bin/aws s3api put-object --bucket $bucket --key cloud_metadata.yaml --body "/tmp/${SILO_ID}_cloud_md.yaml"
+rm -f "/tmp/${SILO_ID}_cloud_md.yaml"
