250998b4e165590677d65edda68e58ac45e2c8c7
Matt Ruddy <matt@calcwise.com>
2023-06-19 18:12:27 +0100

    Pull to stdout if no dest given

--- etc/types/aws/actions/pull.sh
+++ etc/types/aws/actions/pull.sh
@@ -3,4 +3,5 @@ set -e
 test $SILO_PUBLIC = "true" && sign_request=--no-sign-request || sign_request=""
 test $SILO_RECURSIVE = "true" && recursive=--recursive || recursive=""
 object_uri="s3://$SILO_NAME/$SILO_SOURCE"
-$SILO_TYPE_DIR/cli/bin/aws s3 cp "$object_uri" "$SILO_DEST" $sign_request $recursive
+destination=${SILO_DEST:=/dev/stdout}
+$SILO_TYPE_DIR/cli/bin/aws s3 cp "$object_uri" "$destination" $sign_request $recursive
