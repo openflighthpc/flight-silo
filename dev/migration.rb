db6ffcfc95c4cd8d629f68e98d32957f92cc3aa3
Marco Luo <mailforrgscid@gmail.com>
2024-01-09 01:41:10 +0000

    migration#add

--- dev/migration.rb
+++ dev/migration.rb
@@ -13,22 +13,31 @@ class SoftwareMigration
       software_migration.get_archive(archive)
     end
 
+    def merge(repo_software_migration)
+      software_migration.merge(repo_software_migration)
+    end
+
+    def add(item)
+      software_migration.add(item)
+    end
+
   end
 
   attr_reader :enabled_archive
 
   def initialize(file_dir) # file_path = Config.migration_dir
-    unless File.exists?(file_dir)
+    @file_path = File.join(file_dir, 'migration.yml')
+    unless File.exist?(@file_path)
       migration_hash = {
         'enabled_archive' => 'default',
         'items' => []
       }
       `mkdir -p #{file_dir}`
-      File.open(File.join(file_dir, 'migration.yml'), 'w') do |file|
+      File.open(@file_path, 'w') do |file|
         file.write(migration_hash.to_yaml)
       end
     end
-    data = YAML.load_file(File.join(file_dir, 'migration.yml'))
+    data = YAML.load_file(@file_path)
     @enabled_archive = data["enabled_archive"]
     @items = data["items"]
   end
@@ -47,18 +56,46 @@ class SoftwareMigration
     @items.concat(repo_software_migration.items)
   end
 
-end
-
-class RepoSoftwareMigration
-
-  attr_reader :items
-
-  def initialize(repo_id, file_path)
-    raise 'file not exists' unless File.exists?(file_path)
-    @items = YAML.load_file(path)['items']
-    @items.each do |item|
-      item['repo_id'] = repo_id
-    end
+  def add(item)
+    @items << item.to_hash
+    save
   end
 
+  def to_hash()
+    {
+      'enabled_archive' => @enabled_archive,
+      'items' => @items
+    }
+  end
+
+  def save()
+    File.open(@file_path, 'w') do |file|
+      file.write(to_hash.to_yaml)
+    end
+  end
+end
+
+class MigrationItem
+
+  def initialize(type, name, version, path, absolute, repo_id, archive = SoftwareMigration.enabled_archive)
+    @type = type
+    @name = name
+    @version = version
+    @path = path
+    @absolute = absolute
+    @repo_id = repo_id
+    @archive = archive
+  end
+
+  def to_hash
+    {
+      'type' => @type,
+      'name' => @name,
+      'version' => @version,
+      'path' => @path,
+      'absolute' => @absolute,
+      'repo_id' => @repo_id,
+      'archive' => @archive
+    }
+  end
 end
