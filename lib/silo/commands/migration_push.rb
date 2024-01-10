d8ca041a299e8247615286e91e86b17e00500512
Marco Luo <mailforrgscid@gmail.com>
2024-01-10 11:40:12 +0000

    migration class refactor

--- lib/silo/commands/migration_push.rb
+++ lib/silo/commands/migration_push.rb
@@ -33,16 +33,43 @@ module FlightSilo
     class MigrationPush < Command
       def run
         `mkdir -p #{Config.migration_dir}/temp`
+        
+        main = @options.main
+
+        public_repo_items = []
+        private_repo_migrations = {}
         SoftwareMigration.get_repo_migrations.each do |repo_id, repo_migration_hash|
           silo = Silo.fetch_by_id(repo_id)
-          puts "Updating #{silo.name} migration archives..."
-          temp_repo_migration_path = File.join(Config.migration_dir, 'temp', "migration_#{silo.id}.yml")
-          File.open(temp_repo_migration_path, 'w') do |file|
-            file.write(repo_migration_hash.to_yaml)
+          if silo.is_public
+            public_repo_items.concat(repo_migration_hash['items'])
+          else
+            private_repo_migrations[repo_id] = repo_migration_hash
           end
-          silo.push(temp_repo_migration_path, '/migration.yml')
-          File.delete(temp_repo_migration_path)
         end
+        
+        main_archives = SoftwareMigration.list_main_archives
+        restricted_archives = SoftwareMigration.list_restricted_archives
+        restricted_public_archives = []
+        undefined_public_archives = []
+        public_repo_items.each do |item|
+          archive = item['archive']
+          if main_archives.include?(archive)
+            private_repo_migrations[SoftwareMigration.get_main_repo(archive)]['items'] << item 
+          elsif restricted_archives.include?(archive)
+            restricted_public_archives << archive
+          else
+            undefined_public_archives << archive
+          end
+        end
+
+
+          # puts "Updating #{silo.name} migration archives..."
+          # temp_repo_migration_path = File.join(Config.migration_dir, 'temp', "migration_#{silo.id}.yml")
+          # File.open(temp_repo_migration_path, 'w') do |file|
+          #   file.write(repo_migration_hash.to_yaml)
+          # end
+          # silo.push(temp_repo_migration_path, '/migration.yml')
+          # File.delete(temp_repo_migration_path)
         puts Paint["All Done √", :green]  
       end
     end
