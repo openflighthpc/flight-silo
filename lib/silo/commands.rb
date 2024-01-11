39c1142960de1f8ceedbc132bf94ec4c15600adc
Marco Luo <mailforrgscid@gmail.com>
2024-01-11 18:56:35 +0000

    migration pull

--- lib/silo/commands.rb
+++ lib/silo/commands.rb
@@ -47,6 +47,7 @@ require_relative 'commands/migration_switch'
 require_relative 'commands/migration_continue'
 require_relative 'commands/migration_pause'
 require_relative 'commands/migration_push'
+require_relative 'commands/migration_pull'
 require_relative 'commands/migration_apply'
 require_relative 'commands/migration_remove_software'
 
