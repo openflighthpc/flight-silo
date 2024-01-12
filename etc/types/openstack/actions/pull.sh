a937227729a356d5ee379d7cb9125290c28511e5
Matt Ruddy <matt@calcwise.com>
2024-01-12 18:51:17 +0000

    Show STDERR for s3 pull actions

--- etc/types/openstack/actions/pull.sh
+++ etc/types/openstack/actions/pull.sh
@@ -4,4 +4,4 @@ test $SILO_PUBLIC = "true" && sign_request=--no-sign-request || sign_request=""
 test $SILO_RECURSIVE = "true" && recursive=--recursive || recursive=""
 object_uri="s3://$SILO_NAME/$SILO_SOURCE"
 destination=${SILO_DEST:=/dev/stdout}
-$SILO_TYPE_DIR/cli/bin/aws s3 cp "$object_uri" "$destination" $sign_request $recursive --quiet
+$SILO_TYPE_DIR/cli/bin/aws s3 cp "$object_uri" "$destination" $sign_request $recursive --only-show-errors
