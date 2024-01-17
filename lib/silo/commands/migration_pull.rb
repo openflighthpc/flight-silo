7add4aee70e4dfd1061e1c7a74efdcf3b8fb7cf7
Marco Luo <mailforrgscid@gmail.com>
2024-01-17 10:14:16 +0000

    migration pull refactor

--- lib/silo/commands/migration_pull.rb
+++ lib/silo/commands/migration_pull.rb
@@ -43,8 +43,8 @@ module FlightSilo
         silo = Silo[silo_name]
         dest = File.join(Config.migration_dir, 'temp', "migration_#{silo.id}.yml")
         silo.pull('/migration.yml', dest)
-        SoftwareMigration.remove_repo(silo.id)
-        SoftwareMigration.merge(silo.id, YAML.load_file(dest))
+        Migration.remove_repo(silo.id)
+        Migration.add_repo(RepoMigration.new(dest, silo.id))
         File.delete(dest)
 
         puts Paint["Done √", :green]
